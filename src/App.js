148c9cee806ab72753a427b05af2665e6fe64b30
Tim <timtran007@gmail.com>
2022-12-16 12:35:19 -0600

    import Nav component into App.js add H1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Nav from "./Nav"
 
 function App() {
   return (
     <div>
-      
+      <h1 style={{textAlign: "center"}}> Welcome to Dog Adventures</h1>
+      <div>
+        <Nav />
+      </div>
     </div>
   );
 }
