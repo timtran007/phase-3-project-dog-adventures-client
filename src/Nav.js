79a79b09890a46f740ae6d08386c3cd6fa76b472
Tim <timtran007@gmail.com>
2022-12-16 15:10:42 -0600

    add links for home, my dogs and add a dog form

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,18 @@
 import {NavLink} from "react-router-dom"
+import Navbar from "../src/styles/Navbar.css"
 
 function Nav() {
     return (
         <nav>
-            
+            <NavLink to="/">
+                Home
+            </NavLink>
+            <NavLink to="/dogs">
+                My Dogs
+            </NavLink>
+            <NavLink to="/add-a-dog">
+                Add a Dog
+            </NavLink>
         </nav>
     )
 }
