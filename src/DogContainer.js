f5573c3681070ebb3043000cf773b8e2c621ca02
Tim <timtran007@gmail.com>
2022-12-16 19:53:17 -0600

    add ternary statement for DogContainer header

--- src/DogContainer.js
+++ src/DogContainer.js
@@ -1,8 +1,13 @@
 import DogList from "./DogList"
 
 function DogContainer({dogs}) {
+
+    const dogName = dogs.map(dog => dog.name)
+
+    const displayHeader = dogs.length > 1 ? "My Dogs" : {dogName}
     return(
         <div>
+            <h2 style={{textAlign: "center", marginTop: 50}}>{displayHeader}</h2>
             <DogList dogs={dogs}/>
         </div>
     )
