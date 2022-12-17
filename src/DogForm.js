cdfad585ee46ecb3508f03fe72307eabf8d556b9
Tim <timtran007@gmail.com>
2022-12-16 20:43:35 -0600

    add controlled forms feature to inputs

--- src/DogForm.js
+++ src/DogForm.js
@@ -1,6 +1,26 @@
 import AddADog from '../src/styles/AddADog.css'
+import {useState} from 'react'
 
 function DogForm(){
+    const [formData, setFormData] = useState({
+        name: "",
+        breed: "",
+        personality: "",
+        img_url: ""
+    })
+
+    function handleChange(e){
+        const key = e.target.name
+        const value = e.target.value
+        setFormData({
+            ...formData,
+            [key]: value
+        }
+        )
+    }
+    console.log("formData", formData)
+
+
     return(
         <form style={{textAlign: "center"}} id="addADogForm">
             <h2 id="addADogHeader">Add My Dog</h2>
@@ -8,9 +28,11 @@ function DogForm(){
                 <label htmlFor="name">Dog's Name:</label>
                 <p>
                 <input 
+                    onChange={handleChange}
                     type="text" 
                     name="name" 
                     placeholder="Enter dog's name"
+                    value={formData.name}
                 />
                 </p>
             </p>
@@ -18,9 +40,11 @@ function DogForm(){
                 <label htmlFor="breed">Dog's Breed:</label>
                 <p>
                 <input 
+                    onChange={handleChange}
                     type="text" 
                     name="breed" 
                     placeholder="Enter dog's breed"
+                    value={formData.breed}
                 />
                 </p>
             </p>
@@ -28,9 +52,11 @@ function DogForm(){
                 <label htmlFor="personality">Dog's Personality:</label>
                 <p>
                 <input 
+                    onChange={handleChange}
                     type="text" 
                     name="personality" 
                     placeholder="Enter dog's personality"
+                    value={formData.personality}
                 />
                 </p>
             </p>
@@ -38,9 +64,11 @@ function DogForm(){
                 <label htmlFor="img_url">Dog's Image URL:</label>
                 <p>
                 <input 
+                    onChange={handleChange}
                     type="text" 
                     name="img_url" 
                     placeholder="Add an image URL link"
+                    value={formData.img_url}
                 />
                 </p>
             </p>
