c9a2793631e643e3cb7b60d85794b7e4cc75d12c
Tim <timtran007@gmail.com>
2022-12-16 21:40:30 -0600

    render information from state in DogDetails component

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,10 +1,19 @@
 import AdventureCard from "./AdventureCard"
 import AdventureForm from "./AdventureForm"
 
-function DogDetails(){
+function DogDetails({dog}){
     return(
-        <div>
-
+        <div className="card" id={dog.id}>
+            <h3 className="Dog">
+                Dog: {dog.name}
+            </h3>
+            <img className="dogImage" src= {dog.img_url} alt={dog.name} />
+            <h4 className="description">
+                Personality: {dog.personality}
+            </h4>
+            <h4 className="adventures">
+                I have been on {dog.adventures.length} adventures!
+            </h4>
         </div>
     )
 }
