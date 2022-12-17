c9a4776502b2ff63532e772d893ecfea9d4bcb44
Tim <timtran007@gmail.com>
2022-12-17 15:56:10 -0600

    add elements to render in AdventureCard component

--- src/AdventureCard.js
+++ src/AdventureCard.js
@@ -1,7 +1,32 @@
-function AdventureCard(){
+
+function AdventureCard({dog}){
+    debugger
+    const adventuresToDisplay = dog.adventures.map(adventure =>{
+        return(
+            <div className="card">
+                <h3 className="dog">
+                Adventure: {adventure.location}
+                </h3>
+                <img className="dogImage" src= {adventure.image_url} alt={adventure.location} />
+                <h4 className="description">
+                    City: {adventure.city}
+                </h4>
+                <h4 className="description">
+                    Date: {adventure.date}
+                </h4>
+                <h4 className="description">
+                    Rating: {adventure.rating}/5
+                </h4>
+                <h4 className="description">
+                    Notes: {adventure.notes}
+                </h4>
+            </div>
+        )
+    })
+
     return(
         <div>
-
+            {adventuresToDisplay}
         </div>
     )
 }
