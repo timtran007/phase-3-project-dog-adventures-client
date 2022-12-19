c28d8fc8dc231d8d877f9cc6e44d140d5498360e
Tim <timtran007@gmail.com>
2022-12-18 19:35:39 -0600

    add handleChange, handleFormSubmit callback functions, formData as state

--- src/EditForm.js
+++ src/EditForm.js
@@ -1,26 +1,53 @@
+import { useState } from "react"
+function EditForm({adventure, onEditDogAdventure}) {
+    const [formData, setFormData] = useState({
+        date: adventure.date,
+        rating: adventure.rating,
+        notes: adventure.notes
+    })
 
-function EditForm({dog}) {
-    const options = dog.adventures.map(adventure =>
-        <option key={adventure.id} value={adventure.location}>{adventure.location}</option>
-    )
+    // const options = dog.adventures.map(adventure =>
+    //     <option key={adventure.id} value={adventure.location}>{adventure.location}</option>
+    // )
+
+    function handleChange(e){
+        const key = e.target.name
+        const value = e.target.value
+        setFormData({
+            ...formData,
+            [key]: value
+        })
+    }
+
+    function handleFormSubmit(e){
+        e.preventDefault()
+        fetch(`http://localhost:9292/adventures/${adventure.id}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(resp => resp.json())
+        .then(updatedAdventure => {
+            console.log('updatedAventure', updatedAdventure)
+            onEditDogAdventure(updatedAdventure)
+        })
+    }
+    console.log(formData)
 
     return(
-        <form>
+        <form onSubmit={handleFormSubmit}>
             <h4>Edit my adventure:</h4>
-            <div>
-                <select name="selectAdventure">
-                    {options}
-                </select>
-            </div>
                 <div>
                 <label htmlFor="date">Date:</label>
                 <p>
                 <input 
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     type="text" 
                     name="date" 
                     placeholder="Enter YYYY-MM-DD"
-                    // value={formData.}
+                    value={formData.date}
                 />
                 </p>
             </div>
@@ -28,12 +55,12 @@ function EditForm({dog}) {
                 <label htmlFor="rating">Rating `1 (lowest) to 5 (highest)`:</label>
                 <p>
                 <input 
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     type="number" 
                     name="rating" 
-                    min="1"
-                    max="5"
-                    // value={formData.}
+                    max= "5"
+                    min= "1"
+                    value={formData.rating}
                 />
                 </p>
             </div>
@@ -41,16 +68,18 @@ function EditForm({dog}) {
                 <label htmlFor="notes">Notes:</label>
                 <p>
                 <input 
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     type="textarea" 
                     name="notes" 
                     placeholder="Brief summary"
-                    // value={formData.}
+                    value={formData.notes}
                 />
                 </p>
             </div>
+            <div>
+                <input style={{padding: "1%", marginTop: "1%"}}type="submit" value="Save Changes"/>
+            </div>
         </form>
-    )
-}
+)}
 
 export default EditForm
