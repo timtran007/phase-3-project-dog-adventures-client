0b99a62346f503fa4bad841278aa9caf02dac97f
Tim <timtran007@gmail.com>
2022-12-20 13:16:19 -0600

    add formData state, handleChange and handleSubmit callback

--- src/AdventureForm.js
+++ src/AdventureForm.js
@@ -1,17 +1,54 @@
 import AddAnAdventure from '../src/styles/AddAnAdventure.css'
-function AdventureForm(){
+import { useState } from 'react'
+function AdventureForm({dog, onSubmitAdventure}){
+    const [formData, setFormData] = useState({
+        image_url: "",
+        date: "",
+        city: "",
+        location: "",
+        rating: 1,
+        notes: "",
+        dog_id: dog.id
+    })
+
+    function handleChange(e){
+        const key = e.target.name
+        const value = e.target.value
+        setFormData({
+            ...formData,
+            [key]: value
+        })
+        
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        fetch('http://localhost:9292/adventures',{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(resp => resp.json())
+        .then(newAdventure =>{
+            onSubmitAdventure(newAdventure)
+        })
+    }
+
+    console.log(formData)
     return(
-        <form id="addAnAdventureForm">
+        <form onSubmit={handleSubmit} id="addAnAdventureForm">
              <div>
                 <h2 id="addAnAdventureHeader">Add Another Adventure</h2>
                 <label htmlFor="location">Place You Went:</label>
                 <p>
                 <input 
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     type="text" 
                     name="location" 
                     placeholder="Enter location of adventure"
-                    // value={formData.}
+                    value={formData.location}
                 />
                 </p>
             </div>
@@ -19,11 +56,11 @@ function AdventureForm(){
                 <label htmlFor="city">City:</label>
                 <p>
                 <input 
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     type="text" 
                     name="city" 
                     placeholder="Enter city"
-                    // value={formData.}
+                    value={formData.city}
                 />
                 </p>
             </div>
@@ -31,11 +68,11 @@ function AdventureForm(){
                 <label htmlFor="date">Date:</label>
                 <p>
                 <input 
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     type="text" 
                     name="date" 
                     placeholder="Enter YYYY-MM-DD"
-                    // value={formData.}
+                    value={formData.date}
                 />
                 </p>
             </div>
@@ -43,12 +80,12 @@ function AdventureForm(){
                 <label htmlFor="rating">Rating `1 (lowest) to 5 (highest)`:</label>
                 <p>
                 <input 
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     type="number" 
                     name="rating" 
                     min="1"
                     max="5"
-                    // value={formData.}
+                    value={formData.rating}
                 />
                 </p>
             </div>
@@ -56,11 +93,11 @@ function AdventureForm(){
                 <label htmlFor="notes">Notes:</label>
                 <p>
                 <input 
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     type="textarea" 
                     name="notes" 
                     placeholder="Brief summary"
-                    // value={formData.}
+                    value={formData.notes}
                 />
                 </p>
             </div>
@@ -68,11 +105,11 @@ function AdventureForm(){
                 <label htmlFor="image_url">Image Link URL:</label>
                 <p>
                 <input 
-                    // onChange={handleChange}
+                    onChange={handleChange}
                     type="text" 
                     name="image_url" 
                     placeholder="add an image of your adventure"
-                    // value={formData.}
+                    value={formData.image_url}
                 />
                 </p>
             </div>
